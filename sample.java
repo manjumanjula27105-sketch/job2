1780598b52a058068d6941729fc02a9e22103882
manjumanjula27105-sketch <manjumanjula27105@gmail.com>
2026-09-02 16:09:56 +0530

    Add greeting from GitHub to sample.java
    Added a new print statement to display 'hello from github'.

--- sample.java
+++ sample.java
@@ -2,6 +2,8 @@ class sample{
     public static void main(String args[])
     {
         System.out.println("hello from jenkins");
-        System.out.println("hello from my Manju");
+        System.out.println("hello from  Manju");
+          System.out.println("hello from github");
+        
     }
 }
